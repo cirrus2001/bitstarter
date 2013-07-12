629512c475a165691aa1f9539aaca569f65d7145
Ubuntu <ubuntu@ip-10-160-245-122.us-west-1.compute.internal>
2013-07-12 05:35:50 +0000

    a

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
 var express = require('express');
-//var fs = require('fs');
+var fs = require('fs');
 
-//var str = fs.readFileSync('index.html');
+var str = fs.readFileSync('index.html');
 //var buffer = new Buffer(str, 'utf-8');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('sss');
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
